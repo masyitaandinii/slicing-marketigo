3840f426b1b849f36d49a726a8859ea40720c318
masyitaandinii <masyitaandini@gmail.com>
2023-11-06 15:52:14 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+    extend: {
+      theme: {
+        backgroundColor: {
+          'a78bfa': '#a78bfa',
+        },
+        textColor: {
+          'a78bfa': '#a78bfa',
+        },
+    },
+  },
+  plugins: [],
+}
